5e550ece1380521d6e7e4fc31f97cfe37d5b63d6
nikita1803 <rainikita595@gmail.com>
2021-05-31 22:54:57 +0530

    Added: Finding the maximum float value

--- FindMaxNumber.java
+++ FindMaxNumber.java
@@ -2,6 +2,39 @@ package com.genericmaxnumber;
 
 public class FindMaxNumber 
 {
+	/**
+	 * findMaxNumber is a function to check the maximum value in float
+	 * @param floatNum
+	 * @return float value
+	 */
+	public Float findMaxNumber(Float[] floatNum) 
+	{
+		for (int i = 0; i < (floatNum.length - 1); i++) 
+		{
+            for (int j = 0; j < (floatNum.length - 1); j++) 
+            {
+                if (floatNum[j].compareTo(floatNum[j + 1]) < 0) 
+                {
+                    Float temp = floatNum[j];
+                    floatNum[j] = floatNum[j + 1];
+                    floatNum[j + 1] = temp;
+                }
+            }
+        }
+
+        return floatNum[0];
+	}
+		public void printMaxNumber(float max) 
+		{
+		
+			System.out.println("Maximum Value is:" + max);
+		}
+	/**
+	 * findMaxNumber is a function to check the maximum value in integer.
+	 * @param intArray
+	 * @return integer value
+	 */
+		
 	public Integer findMaxNumber(Integer[] intArray) 
 	{
 		for (int i = 0; i < (intArray.length - 1); i++) 
@@ -19,9 +52,10 @@ public class FindMaxNumber
 
         return intArray[0];
 	}
-
-	public void printMaxNumber(Integer max) {
-	
+	public void printMaxNumber(Integer max) 
+	{
 		System.out.println("Maximum Value is:" + max);
 	}
+
+
 }
--- MaxNumberTest.java
+++ MaxNumberTest.java
@@ -10,12 +10,20 @@ public class MaxNumberTest
 	FindMaxNumber maxNumber = new FindMaxNumber();
 	
 	@Test
-	public void returnMaximumValue() {
+	public void returnMaximumValueInteger() {
         Integer[] intArray = {3,8,4};
         maxNumber = new FindMaxNumber();
         Integer maxInteger = maxNumber.findMaxNumber(intArray);
         maxNumber.printMaxNumber(maxInteger);
         Assert.assertEquals(Integer.valueOf(8), maxInteger);
+    }
+	@Test
+    public void returnMaximumValueFloat() {
+        Float[] floatNum = {1.1f , 1.2f , 1.5f};
+        maxNumber = new FindMaxNumber();
+        Float maxFloat = maxNumber.findMaxNumber(floatNum);
+        maxNumber.printMaxNumber(maxFloat);
+        Assert.assertEquals(Float.valueOf(1.2f), maxFloat);
     }
 
 }
